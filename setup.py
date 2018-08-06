4c919e37599edf4a5f0530d604180998a70a1c38
Allison Seiden <ahseiden@gmail.com>
2018-08-06 17:02:06 -0400

    modified console_scripts

--- setup.py
+++ setup.py
@@ -19,9 +19,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ),
     entry_points={
-        "console_scripts": ['sorting_hat=sorting_hat.sorting_hat:main',
-        'sorting-hat=sorting-hat.sorting-hat:main'
-        ]
+        "console_scripts": ['sorting_hat=sorting_hat.sorting_hat:main','sorting-hat=sorting-hat.sorting-hat:main', 'sortinghat=sortinghat.sortinghat:main']
     },
     install_requires=[
           'pandas', 'numpy'

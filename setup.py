ca713cdd577e56bb19e2e579307409ec26f59bc1
Allison Seiden <ahseiden@gmail.com>
2018-08-06 11:51:07 -0400

    adding files for uploading to pypi

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sorting_hat",
+    version="0.0.1",
+    author="Allison Seiden",
+    author_email="ahseiden@gmail.com",
+    description="Sorts indels into mutational classes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/allisonseiden/sorting_hat",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
---
+++ sorting_hat/__init__.py
@@ -0,0 +1,1 @@
+name = "sorting_hat"
